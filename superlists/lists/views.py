da4a6bfbf934c80a967c9bcdc36dfc744f3377df
hyesun <chsun0303@gmail.com>
2016-09-22 05:14:43 +0900

    POST 후에 리디렉션과 템플릿에 모든 작업 아이템 표시

---
+++ superlists/lists/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from .models import Item
+
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
