d9a2e25cf4bd48cac02bac6e8ef4a1ec7a78d7b9
hyesun <chsun0303@gmail.com>
2016-09-23 15:56:21 +0900

    레이아웃 개선을 위한 부트스트랩 사용

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest, time, os, sys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         # caps = DesiredCapabilities.FIREFOX
