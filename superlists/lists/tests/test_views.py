ed04972eee1a271dbd9d73494bc964c4782e31ac
hyesun03 <chsun0303@gmail.com>
2016-09-27 18:50:56 +0900

    단위 테스트를 두 개 파일로 나누기

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -4,8 +4,8 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 import re
 
-from .views import home_page
-from .models import Item, List
+from lists.views import home_page
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -25,33 +25,6 @@ class HomePageTest(TestCase):
         response_decode = self.remove_csrf(response.content.decode())
 
         self.assertEqual(response_decode, expected_html)
-
-class ListAndItemModelText(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = '첫 번째 아이템'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = '두 번째 아이템'
-        second_item.list = list_
-        second_item.save()
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, '첫 번째 아이템')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, '두 번째 아이템')
-        self.assertEqual(second_saved_item.list, list_)
 
 class ListViewTest(TestCase):
 
